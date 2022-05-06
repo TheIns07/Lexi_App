a578839ab123edc2322cd32446053d20b46c82df
Arturo Cervantes <jesus.cervantes207003@potros.itson.edu.mx>
2022-05-05 22:22:12 -0600

    Proyecto integrado con APK

--- app/src/main/java/apps/project/lexi_app/ui/themes/FillFragment.kt
+++ app/src/main/java/apps/project/lexi_app/ui/themes/FillFragment.kt
@@ -1,4 +1,43 @@
 package apps.project.lexi_app.ui.themes
 
-class FillFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import apps.project.lexi_app.R
+import apps.project.lexi_app.databinding.CompleteTextBinding
+
+class FillFragment : Fragment(){
+
+    private var _binding: CompleteTextBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = CompleteTextBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnBack.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .remove(this).commit()
+        }
+
+        binding.buttonGridConfirm.setOnClickListener {
+            onDestroyView()
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.nav_host_fragment_activity_main,GridVoiceFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
 }
--- app/src/main/java/apps/project/lexi_app/ui/themes/GridVoiceFragment.kt
+++ app/src/main/java/apps/project/lexi_app/ui/themes/GridVoiceFragment.kt
@@ -1,4 +1,39 @@
 package apps.project.lexi_app.ui.themes
 
-class GridVoiceFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import apps.project.lexi_app.R
+import apps.project.lexi_app.databinding.GridVoiceBinding
+
+class GridVoiceFragment: Fragment() {
+
+    private var _binding: GridVoiceBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = GridVoiceBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.btnBack.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .remove(this).commit()
+        }
+
+        binding.buttonGridConfirm.setOnClickListener {
+
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.nav_host_fragment_activity_main,FillFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+
+    }
 }
